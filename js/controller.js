d77daa43d2fb39c17f403f0e02e2d643ea81cf11
Steve Geluso <stevegeluso@gmail.com>
2014-05-20 12:00:03 -0700

    removing left/right

--- js/controller.js
+++ js/controller.js
@@ -64,10 +64,6 @@ app.controller('Controller', ['$scope', function($scope) {
     QUICK_STEAL = newValue;
   });
 
-  $scope.$watch('onlyCurrentCursor', function(newValue, oldValue) {
-    ONLY_CURRENT_CURSOR = newValue;
-  });
-
   $scope.$watch('record', function(newValue, oldValue) {
     RECORD = newValue;
   });
@@ -79,19 +75,4 @@ app.controller('Controller', ['$scope', function($scope) {
       BACKGROUND_IMAGE = img;
     };
   }
-
-  $scope.leftPoints = 0;
-  $scope.rightPoints = 0;
-
-  setInterval(function() {
-    if ((FOLDER.x + FOLDER.WIDTH / 2) < WIDTH / 2 - 10) {
-      $scope.$apply(function() {
-        $scope.leftPoints++;
-      });
-    } else if ((FOLDER.x + FOLDER.HEIGHT / 2) > WIDTH / 2 + 10) {
-      $scope.$apply(function() {
-        $scope.rightPoints++;
-      });
-    }
-  }, FRAMERATE);
 }]);
